ee9fcbb68bba1fb21a346b59cfc7bea409c1029e
Keval <keval@thechubbypanda.net>
2020-12-18 23:02:51 +0000

    Found a neater way to do day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,23 +4,24 @@ fn main() {
 	let input = std::fs::read_to_string("input.txt").unwrap();
 
 	let timer = Instant::now();
-	println!("Part 1 output: {}", part1(&input, 3, 1));
+	println!("Part 1 output: {}", part1(&input, (3, 1)));
 	println!("Part 1 time: {:?}", timer.elapsed());
 	let timer = Instant::now();
 	println!("Part 2 output: {}", part2(&input));
 	println!("Part 2 time: {:?}", timer.elapsed());
 }
 
-fn part1(input: &String, down: usize, right: usize) -> usize {
+fn part1(input: &String, (x, y): (usize, usize)) -> usize {
 	let mut current = 0;
-	return input.lines().enumerate()
-		.filter(|l| (*l).0 % right == 0)
+	return input.lines()
+		.step_by(y)
+		.enumerate()
 		.map(|l| -> Vec<char> {
 			l.1.chars().collect()
 		})
 		.map(|row| -> usize {
-			current += down;
-			if row[(current - down) % row.len()] == '#' {
+			current += x;
+			if row[(current - x) % row.len()] == '#' {
 				return 1;
 			}
 			return 0;
@@ -30,10 +31,10 @@ fn part1(input: &String, down: usize, right: usize) -> usize {
 
 fn part2(input: &String) -> usize {
 	let mut totals = vec!();
-	totals.push(part1(input, 1, 1));
-	totals.push(part1(input, 3, 1));
-	totals.push(part1(input, 5, 1));
-	totals.push(part1(input, 7, 1));
-	totals.push(part1(input, 1, 2));
+	totals.push(part1(input, (1, 1)));
+	totals.push(part1(input, (3, 1)));
+	totals.push(part1(input, (5, 1)));
+	totals.push(part1(input, (7, 1)));
+	totals.push(part1(input, (1, 2)));
 	return totals.iter().fold(1, |a, v| a * v);
 }
