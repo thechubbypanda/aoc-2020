358a72618980742e7a5175883a07b899ac8aad5b
Keval Kapdee <keval@thechubbypanda.net>
2020-12-23 16:14:51 +0000

    Day 10 part 1 result return

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,29 +5,30 @@ fn main() {
 	let adapters: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
 
 	let timer = Instant::now();
-	println!("Part 1 output: {}", part1(adapters.clone()));
+	println!("Part 1 output: {}", part1(adapters.clone()).ok().unwrap());
 	println!("Part 1 time: {:?}", timer.elapsed());
 }
 
-fn part1(mut input: Vec<u32>) -> usize {
+fn part1(mut input: Vec<u32>) -> Result<u32, u32> {
 	input.sort();
 
 	let mut count_1 = 0;
 	let mut count_3 = 1;
 	let mut last = 0;
 
-	input.iter()
-		.for_each(|a| match a - last {
+	for a in input {
+		match a - last {
 			1 => {
-				last = *a;
+				last = a;
 				count_1 += 1;
 			}
 			3 => {
-				last = *a;
+				last = a;
 				count_3 += 1;
 			}
-			_ => panic!("wtf is {}", a),
-		});
+			e => return Err(e),
+		}
+	}
 
-	count_3 * count_1
+	Ok(count_3 * count_1)
 }
