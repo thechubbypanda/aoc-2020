f00a1fd9d1b79447dc74aa09c4e297793f1c126a
Keval Kapdee <keval@thechubbypanda.net>
2020-12-23 15:37:39 +0000

    Day 9 part 2 slowww

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -10,6 +10,9 @@ fn main() {
 	let timer = Instant::now();
 	println!("Part 1 output: {}", part1(sequence.clone()));
 	println!("Part 1 time: {:?}", timer.elapsed());
+	let timer = Instant::now();
+	println!("Part 2 output: {}", part2(sequence.clone()));
+	println!("Part 2 time: {:?}", timer.elapsed());
 }
 
 fn part1(input: Vec<u64>) -> u64 {
@@ -21,4 +24,22 @@ fn part1(input: Vec<u64>) -> u64 {
 		)
 		.map(|win| win[PREAMBLE])
 		.unwrap()
+}
+
+fn part2(input: Vec<u64>) -> u64 {
+	let num = part1(input.clone());
+	let set: Vec<u64> = input.iter().take_while(|v| **v != num).map(|u| *u).collect();
+	let size = set.clone().len();
+	for start in 0..size {
+		for end in (start + 1)..size {
+			if input[start..=end].iter().sum::<u64>() == num {
+				let largest = input[start..=end].iter()
+					.fold(u64::min_value(), |a, v| if *v > a { *v } else { a });
+				let smallest = input[start..=end].iter()
+					.fold(u64::max_value(), |a, v| if *v < a { *v } else { a });
+				return smallest + largest;
+			}
+		}
+	}
+	0
 }
