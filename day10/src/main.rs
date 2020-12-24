ee34323ec04eff7a0cadb98496a424a654bd2db8
Keval Kapdee <keval@thechubbypanda.net>
2020-12-24 17:03:21 +0000

    Day 10 finalized part 1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,17 +1,16 @@
 use std::time::Instant;
+use itertools::Itertools;
 
 fn main() {
 	let input = std::fs::read_to_string("input.txt").unwrap();
-	let adapters: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
+	let adapters: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).sorted().collect();
 
 	let timer = Instant::now();
-	println!("Part 1 output: {}", part1(adapters.clone()).ok().unwrap());
+	println!("Part 1 output: {}", part1(adapters.clone()));
 	println!("Part 1 time: {:?}", timer.elapsed());
 }
 
-fn part1(mut input: Vec<u32>) -> Result<u32, u32> {
-	input.sort();
-
+fn part1(input: Vec<u32>) -> u32 {
 	let mut count_1 = 0;
 	let mut count_3 = 1;
 	let mut last = 0;
@@ -26,9 +25,9 @@ fn part1(mut input: Vec<u32>) -> Result<u32, u32> {
 				last = a;
 				count_3 += 1;
 			}
-			e => return Err(e),
+			_ => unreachable!(),
 		}
 	}
 
-	Ok(count_3 * count_1)
+	count_3 * count_1
 }
