3866137d2cb9726b3b01bc0813db306a06db68b4
Keval Kapdee <keval@thechubbypanda.net>
2020-12-24 19:17:22 +0000

    Day 10 part 2 wtf why

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
 use itertools::Itertools;
+use std::collections::HashMap;
 
 fn main() {
 	let input = std::fs::read_to_string("input.txt").unwrap();
@@ -8,26 +9,46 @@ fn main() {
 	let timer = Instant::now();
 	println!("Part 1 output: {}", part1(adapters.clone()));
 	println!("Part 1 time: {:?}", timer.elapsed());
+	let timer = Instant::now();
+	println!("Part 2 output: {}", part2(adapters.clone()));
+	println!("Part 2 time: {:?}", timer.elapsed().as_secs_f64());
 }
 
 fn part1(input: Vec<u32>) -> u32 {
 	let mut count_1 = 0;
 	let mut count_3 = 1;
-	let mut last = 0;
 
-	for a in input {
-		match a - last {
-			1 => {
-				last = a;
-				count_1 += 1;
-			}
-			3 => {
-				last = a;
-				count_3 += 1;
-			}
-			_ => unreachable!(),
-		}
-	}
+	let mut input = input;
+	input.insert(0, 0);
+
+	input.iter()
+		.tuple_windows()
+		.for_each(|(p, n)| match n - p {
+			1 => count_1 += 1,
+			3 => count_3 += 1,
+			v => unreachable!(v),
+		});
 
 	count_3 * count_1
+}
+
+fn part2(input: Vec<u32>) -> usize {
+	dp(&input, 0, &mut HashMap::new())
+}
+
+fn dp(input: &Vec<u32>, start: usize, stored: &mut HashMap<usize, usize>) -> usize {
+	if start == input.len() - 1 {
+		return 1;
+	}
+	if let Some((_, v)) = stored.iter().find(|(i, _)| start == **i) {
+		return *v;
+	}
+	let mut ans = 0;
+	for i in (start+1)..input.len() {
+		if input[i] - input[start] <= 3 {
+			ans += dp(input, i, stored);
+		}
+	}
+	stored.insert(start, ans);
+	ans
 }
